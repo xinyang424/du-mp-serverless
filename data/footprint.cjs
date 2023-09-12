89d436fecf8768a95b4a89a10e2d21e6389e0007
coder-new <2578417052@qq.com>
2023-09-12 09:20:51 +0800

    docs📝: 更新足迹

--- data/footprint.cjs
+++ data/footprint.cjs
@@ -1,6 +1,16 @@
 const footprint = [
   {
     id: 1,
+    name: "苏稽古镇",
+    address: "四川省乐山市市中区半边街12号",
+    longitude: 103.665957,
+    latitude: 29.58978,
+    iconPath: "/static/icon_img/location.png",
+    width: 50,
+    height: 50,
+  },
+  {
+    id: 2,
     name: "乐山大佛风景区",
     address: "四川省乐山市市中区凌云路2435号",
     longitude: 103.774401,
@@ -10,7 +20,7 @@ const footprint = [
     height: 50,
   },
   {
-    id: 2,
+    id: 3,
     name: "张公桥好吃街",
     address: "四川省乐山市市中区张公桥街37号",
     longitude: 103.768295,
@@ -20,7 +30,7 @@ const footprint = [
     height: 50,
   },
   {
-    id: 3,
+    id: 4,
     name: "世豪广场（乐山市）",
     address: "四川省乐山市市中区三苏路与鹤翔路交叉口西侧",
     longitude: 103.726423,
@@ -30,7 +40,7 @@ const footprint = [
     height: 50,
   },
   {
-    id: 4,
+    id: 5,
     name: "成都大源银泰城",
     address: "四川省成都市武侯区益州大道中段1999号",
     longitude: 104.058127,
@@ -40,7 +50,7 @@ const footprint = [
     height: 50,
   },
   {
-    id: 5,
+    id: 6,
     name: "龙湖成都金楠天街",
     address: "四川省成都市武侯区晋阳路313号",
     longitude: 103.996095,
@@ -50,7 +60,7 @@ const footprint = [
     height: 50,
   },
   {
-    id: 6,
+    id: 7,
     name: "凯德天府",
     address: "四川省成都市武侯区天仁路388号",
     longitude: 104.069153,
@@ -60,7 +70,7 @@ const footprint = [
     height: 50,
   },
   {
-    id: 7,
+    id: 8,
     name: "大源中央公园",
     address: "四川省成都市武侯区天府二街777号",
     longitude: 104.04942,
@@ -70,7 +80,7 @@ const footprint = [
     height: 50,
   },
   {
-    id: 8,
+    id: 9,
     name: "南湖公园",
     address: "四川省成都市双流区华阳通济桥下街219号正东方向130米",
     longitude: 104.048286,
@@ -80,7 +90,7 @@ const footprint = [
     height: 50,
   },
   {
-    id: 9,
+    id: 10,
     name: "锦城湖公园",
     address: "四川省成都市武侯区益州大道锦悦西路口(近新世纪环球中心)",
     longitude: 104.048421,
@@ -90,11 +100,21 @@ const footprint = [
     height: 50,
   },
   {
-    id: 10,
+    id: 11,
     name: "大源步行街",
     address: "四川省成都市武侯区大源南一街与大源街步行街交叉口南100米",
     longitude: 104.03317,
     latitude: 30.552829,
+    iconPath: "/static/icon_img/location.png",
+    width: 50,
+    height: 50,
+  },
+  {
+    id: 12,
+    name: "伊藤洋华堂(乐山店)",
+    address: "四川省乐山市市中区通江街道鹤翔路8号",
+    longitude: 103.728156,
+    latitude: 29.601059,
     iconPath: "/static/icon_img/location.png",
     width: 50,
     height: 50,
