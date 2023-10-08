c4febd5d30a120aa16bffa7fb9948fd0e5d1da99
coder-new <2578417052@qq.com>
2023-10-08 16:13:09 +0800

    publish🚀: 后台实现数据库查询

--- config/mp-config.cjs
+++
@@ -1,6 +0,0 @@
-const mp_config = {
-  appid: "wxf121e438321d85e7",
-  secret: "4eec6d8ce505ff761c39ac1c0bf8bbdb",
-};
-
-module.exports = mp_config;
--- router/home.cjs
+++ router/home.cjs
@@ -1,60 +1,89 @@
 const express = require("express");
-const footprint = require("../data/footprint.cjs");
-const trends = require("../data/trends.cjs");
-const menu = require("../data/menu.cjs");
-const openid = require("../data/openid.cjs");
-const function_docs = require("../data/function-docs.cjs");
-const mp_config = require("../config/mp-config.cjs");
-// const axios = require("axios");
+const dayjs = require("dayjs");
+
+const openid = require("../config/openid.cjs");
+const Storage_Bucket_Url = require("../config/url.cjs");
+const db = require("../utils/db.cjs");
+
 const router = express.Router();
 
 router.get("/getdata", function (req, res) {
-  return res.send({
-    code: 200,
-    errMsg: "request:ok",
-    data: {
-      footprint: [...footprint],
-      trends: [...trends],
-      menu: [...menu],
-      openid: openid,
-      function_docs: function_docs,
-      user_openid: req.headers["x-wx-source"] ? req.headers["x-wx-openid"] : "",
-    },
+  const footprint_sql = "SELECT name, address, longitude, latitude FROM footprint;";
+  const trends_sql = "SELECT users.avatar, users.`name` AS 'title', trends.time, trends.tags, trends.content, trends.img_list as 'images' FROM trends LEFT JOIN users ON trends.user_id = users.id;";
+  const menu_sql =
+    "SELECT dish.name, dish.food_ingredients, url as 'img',menu_type AS 'type',menu_id FROM dish LEFT JOIN image on dish.img_id = image.id LEFT JOIN menu ON dish.menu_id = menu.id ORDER BY menu_id;";
+  const function_docs_sql = "SELECT * FROM function_docs;";
+  const query_img_sql = "SELECT url FROM image WHERE id IN(?);";
+
+  db(footprint_sql, null, function (_, footprintData) {
+    db(trends_sql, null, function (_, trendsData) {
+      for (let i = 0; i < trendsData.length; i++) {
+        trendsData[i].time = dayjs(trendsData[i].time).format("YYYY-MM-DD");
+        db(query_img_sql, [trendsData[i].images.split(",")], function (_, imgData) {
+          trendsData[i].images = [];
+          for (let j = 0; j < imgData.length; j++) {
+            trendsData[i].images.push(Storage_Bucket_Url + imgData[j].url);
+          }
+          if (i + 1 == trendsData.length) {
+            db(function_docs_sql, null, function (_, function_docsData) {
+              db(menu_sql, null, function (_, menuData) {
+                return res.send({
+                  code: 200,
+                  errMsg: "request:ok",
+                  data: {
+                    footprint: [...footprintData],
+                    trends: [...trendsData],
+                    menu: handleMenuData(menuData),
+                    openid: openid,
+                    function_docs: function_docsData,
+                    user_openid: req.headers["x-wx-source"] ? req.headers["x-wx-openid"] : "",
+                  },
+                });
+              });
+            });
+          }
+        });
+      }
+    });
   });
 });
 
-// router.post("/getuseropenid", (req, res) => {
-//   axios
-//     .get("https://api.weixin.qq.com/sns/jscode2session", {
-//       params: {
-//         appid: appid,
-//         secret: secret,
-//         appid: mp_config.appid,
-//         secret: mp_config.secret,
-//         js_code: req.body.js_code,
-//         grant_type: "authorization_code",
-//       },
-//     })
-//     .then(response => {
-//       if (response.data.errcode) {
-//         res.send({
-//           code: 500,
-//           errMsg: response.data.errmsg,
-//           data: {},
-//         });
-//       } else {
-//         res.send({
-//           code: 200,
-//           errMsg: "request:ok",
-//           data: response.data,
-//         });
-//       }
-//     })
-//     .catch(error => {
-//       res.send({
-//         code: 500,
-//         errMsg: error,
-//       });
-//     });
-// });
+function handleMenuData(data) {
+  let menuData = [];
+  for (let i = 0; i < data.length; i++) {
+    if (menuData.length) {
+      for (let j = 0; j < menuData.length; j++) {
+        if (data[i].menu_id == menuData[j].menu_id) {
+          menuData[j].foods.push({
+            id: `${menuData.length}-${menuData[j].foods.length + 1}`,
+            name: data[i].img,
+            food_ingredients: data[i].food_ingredients,
+          });
+        } else if (j + 1 == menuData.length) {
+          menuData.push({
+            id: menuData.length + 1,
+            type: data[i].type,
+            menu_id: data[i].menu_id,
+            foods: [],
+          });
+        }
+      }
+    } else {
+      menuData.push({
+        id: menuData.length + 1,
+        type: data[i].type,
+        menu_id: data[i].menu_id,
+        foods: [
+          {
+            id: `${menuData.length + 1}-${menuData[i]?.foods.length || 0 + 1}`,
+            name: data[i].img,
+            food_ingredients: data[i].food_ingredients,
+          },
+        ],
+      });
+    }
+  }
+  return menuData;
+}
+
 module.exports = router;
