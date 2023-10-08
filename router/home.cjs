7a9977b9669c42b2d4dd8d7eed3b4528e425633b
coder-new <2578417052@qq.com>
2023-10-08 16:45:45 +0800

    patch🚑: 修改接口返回数据

--- router/home.cjs
+++ router/home.cjs
@@ -8,17 +8,24 @@ const db = require("../utils/db.cjs");
 const router = express.Router();
 
 router.get("/getdata", function (req, res) {
-  const footprint_sql = "SELECT name, address, longitude, latitude FROM footprint;";
-  const trends_sql = "SELECT users.avatar, users.`name` AS 'title', trends.time, trends.tags, trends.content, trends.img_list as 'images' FROM trends LEFT JOIN users ON trends.user_id = users.id;";
+  const footprint_sql = "SELECT id, name, address, longitude, latitude FROM footprint;";
+  const trends_sql =
+    "SELECT trends.id, users.avatar, users.`name` AS 'title', trends.time, trends.tags, trends.content, trends.img_list as 'images' FROM trends LEFT JOIN users ON trends.user_id = users.id;";
   const menu_sql =
     "SELECT dish.name, dish.food_ingredients, url as 'img',menu_type AS 'type',menu_id FROM dish LEFT JOIN image on dish.img_id = image.id LEFT JOIN menu ON dish.menu_id = menu.id ORDER BY menu_id;";
   const function_docs_sql = "SELECT * FROM function_docs;";
   const query_img_sql = "SELECT url FROM image WHERE id IN(?);";
 
   db(footprint_sql, null, function (_, footprintData) {
+    for (let i = 0; i < footprintData.length; i++) {
+      footprintData[i].iconPath = "/static/icon_img/location.png";
+      footprintData[i].width = 50;
+      footprintData[i].height = 50;
+    }
     db(trends_sql, null, function (_, trendsData) {
       for (let i = 0; i < trendsData.length; i++) {
         trendsData[i].time = dayjs(trendsData[i].time).format("YYYY-MM-DD");
+        trendsData[i].avatar = Storage_Bucket_Url + trendsData[i].avatar;
         db(query_img_sql, [trendsData[i].images.split(",")], function (_, imgData) {
           trendsData[i].images = [];
           for (let j = 0; j < imgData.length; j++) {
@@ -56,7 +63,8 @@ function handleMenuData(data) {
         if (data[i].menu_id == menuData[j].menu_id) {
           menuData[j].foods.push({
             id: `${menuData.length}-${menuData[j].foods.length + 1}`,
-            name: data[i].img,
+            name: data[i].name,
+            img: Storage_Bucket_Url + data[i].img,
             food_ingredients: data[i].food_ingredients,
           });
         } else if (j + 1 == menuData.length) {
@@ -76,7 +84,8 @@ function handleMenuData(data) {
         foods: [
           {
             id: `${menuData.length + 1}-${menuData[i]?.foods.length || 0 + 1}`,
-            name: data[i].img,
+            name: data[i].name,
+            img: Storage_Bucket_Url + data[i].img,
             food_ingredients: data[i].food_ingredients,
           },
         ],
--- utils/db.cjs
+++ utils/db.cjs
@@ -2,13 +2,11 @@ let mysql = require("mysql");
 
 function db(sql, options, callback) {
   let myserver = mysql.createConnection({
-    host: "sh-cynosdbmysql-grp-6i42fo0o.sql.tencentcdb.com",
-    // host: "10.38.104.19",
+    host: process.env.NODE_ENV === "local" ? "sh-cynosdbmysql-grp-6i42fo0o.sql.tencentcdb.com" : "10.38.104.19",
     user: "root",
     password: "rootYX123",
     database: "duDB",
-    port: 20563,
-    // port: 3306,
+    port: process.env.NODE_ENV === "local" ? 20563 : 3306,
   });
 
   myserver.connect();
